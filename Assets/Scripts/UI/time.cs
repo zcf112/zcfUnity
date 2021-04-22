116159100e8e4e3fa8aa26729f4d5c6ee7477edb
zcf112 <1909901740@qq.com>
2021-04-22 13:19:32 +0800

    添加时间相关（按钮）
    添加时间相关（按钮）
    删除时间相关（键盘012控制）

---
+++ Assets/Scripts/UI/time.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ * 时间控制，数字键012控制时间倍率，但只会控制事件相关函数如Update和 LateUpdate，无法控制帧函数（fixupdate）等
+ * 
+ */
+public class time : MonoBehaviour
+{
+    public Button TimeManager;
+    public Text TextTime;
+    private float timer = 0f;
+    void Start()
+    {
+        timer = Time.realtimeSinceStartup;
+        TimeManager.onClick.AddListener(ChangeTime);
+    }
+
+    void ChangeTime()//修改时间倍率
+    {
+        Time.timeScale = int.Parse(TextTime.text);
+    }
+}
